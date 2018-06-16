8b4b3f414ff35c322651c422bb7e59d61378e9d5
weiaicunzai <372995411@qq.com>
2018-06-16 20:16:52 +0800

    fcn32s net complete

--- models/fcn32s.py
+++ models/fcn32s.py
@@ -12,7 +12,7 @@ class FCN32s(nn.Module):
             nn.ReLU(inplace=True),
             nn.Conv2d(64, 64, 3, padding=1),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(2, stride=2) # 1/2
+            nn.MaxPool2d(2, stride=2, ceil_mode=True) # 1/2
         )
 
         self.conv_2 = nn.Sequential(
@@ -20,7 +20,7 @@ class FCN32s(nn.Module):
             nn.ReLU(inplace=True),
             nn.Conv2d(128, 128, 3, padding=1),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(2, stride=2) # 1/4
+            nn.MaxPool2d(2, stride=2, ceil_mode=True) # 1/4
         )
 
         self.conv_3 = nn.Sequential(
@@ -30,7 +30,7 @@ class FCN32s(nn.Module):
             nn.ReLU(inplace=True),
             nn.Conv2d(256, 256, 3, padding=1),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(2, stride=2) # 1/8
+            nn.MaxPool2d(2, stride=2, ceil_mode=True) # 1/8
         )
 
         self.conv_4 = nn.Sequential(
@@ -38,8 +38,9 @@ class FCN32s(nn.Module):
             nn.ReLU(inplace=True),
             nn.Conv2d(512, 512, 3, padding=1),
             nn.ReLU(inplace=True),
-            nn.Conv2d(512, 512, 3, padding=1)
-            nn.MaxPool2d(2, stride=2) # 1/16
+            nn.Conv2d(512, 512, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, stride=2, ceil_mode=True) # 1/16
         )
 
         self.conv_5 = nn.Sequential(
@@ -48,28 +49,50 @@ class FCN32s(nn.Module):
             nn.Conv2d(512, 512, 3, padding=1),
             nn.ReLU(inplace=True),
             nn.Conv2d(512, 512, 3, padding=1),
-            nn.MaxPool2d(2, stride=2) #  1/ 32
+            nn.MaxPool2d(2, stride=2, ceil_mode=True) #  1/ 32
         )
 
         #convolutionailzed fc6
         self.conv_6 = nn.Sequential(
-            
+            nn.Conv2d(512, 4096, 7),
+            nn.ReLU(inplace=True),            
+            nn.Dropout2d()
         )
 
+        #convolutionized fc7
+        self.conv_7 = nn.Sequential(
+            nn.Conv2d(4096, 4096, 1),
+            nn.ReLU(inplace=True),
+            nn.Dropout2d()
+        )
+
+        self.score = nn.Sequential(
+            nn.Conv2d(4096, n_class, 1)
+        )
+
+        #transpose convolution
+        self.upscore = nn.ConvTranspose2d(n_class, n_class, 64, stride=32, bias=False)
 
 
-#deconvolution = nn.ConvTranspose2d(1, 1, 2, stride=2, padding=0, bias=False)
-#for p in deconvolution.parameters():
-#    p = torch.ones(2, 2)
-#    print(p)
+    def forward(self, x):
+        output = self.conv_1(x)
+        output = self.conv_2(output)
+        output = self.conv_3(output)
+        output = self.conv_4(output)
+        output = self.conv_5(output)
+        output = self.conv_6(output)
+        output = self.conv_7(output)
+        output = self.score(output)
+        output = self.upscore(output)
+
+        output = output[:, :, 19 : 19 + x.size()[2], 19:19 + x.size()[3]]
+        return output
+
+
+
+#image = torch.Tensor(3, 300, 500)
+#print(image.size())
 #
-##convolution = nn.Conv2d(1, 1, 2, stride)
-#image = torch.Tensor(4).view(2, 2)
-#image[0, 0] = 1
-#image[0, 1] = 2
-#image[1, 0] = 3
-#image[1, 1] = 4
-#print(image)
-#
-#print(deconvolution(Variable(image.view(1, 1, 2, 2))))
-#
+#fcn32s= FCN32s()
+#output = fcn32s(Variable(image.view(1, 3, 300, 500)))
+#print(output.size())
